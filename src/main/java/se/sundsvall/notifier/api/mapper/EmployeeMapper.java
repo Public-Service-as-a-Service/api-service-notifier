b6618e1cd77716590967dd19074bcfa85a858819
Simon-n-o <simon.n.olsson@gmail.com>
2026-02-09 14:14:02 +0100

    #28 Adds endpoints to EmployeeResource
    Adds endpoints to employee resource + tests

--- src/main/java/se/sundsvall/notifier/api/mapper/EmployeeMapper.java
+++ src/main/java/se/sundsvall/notifier/api/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package se.sundsvall.notifier.api.mapper;
 
 import org.springframework.stereotype.Component;
 import se.sundsvall.notifier.api.model.response.EmployeeResponse;
+import se.sundsvall.notifier.api.model.response.EmployeeWithOrgNameResponse;
 import se.sundsvall.notifier.integration.db.entity.Employee;
 
 @Component
@@ -17,6 +18,21 @@ public class EmployeeMapper {
 			.withWorkPhone(employee.getWorkPhone())
 			.withWorkMobile(employee.getWorkMobile())
 			.withWorkTitle(employee.getWorkTitle())
+			.build();
+	}
+
+	public EmployeeWithOrgNameResponse toResponseWithOrgName(Employee employee) {
+		var orgName = employee.getOrganization().getName();
+		return EmployeeWithOrgNameResponse.builder()
+			.withPersonId(employee.getPersonId())
+			.withOrgId(employee.getOrgId())
+			.withFirstName(employee.getFirstName())
+			.withLastName(employee.getLastName())
+			.withEmail(employee.getEmail())
+			.withWorkPhone(employee.getWorkPhone())
+			.withWorkMobile(employee.getWorkMobile())
+			.withWorkTitle(employee.getWorkTitle())
+			.withOrgName(orgName)
 			.build();
 	}
 }
--- src/main/java/se/sundsvall/notifier/service/EmployeeService.java
+++ src/main/java/se/sundsvall/notifier/service/EmployeeService.java
@@ -1,11 +1,13 @@
 package se.sundsvall.notifier.service;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 import se.sundsvall.notifier.api.mapper.EmployeeMapper;
-import se.sundsvall.notifier.api.model.response.EmployeeResponse;
+import se.sundsvall.notifier.api.model.response.EmployeeWithOrgNameResponse;
 import se.sundsvall.notifier.integration.repository.EmployeeRepository;
 
 @Service
@@ -19,31 +21,27 @@ public class EmployeeService {
 		this.employeeMapper = employeeMapper;
 	}
 
-	public List<EmployeeResponse> getEmployeesByOrg(String orgId) {
-		if (orgId == null || orgId.isBlank()) {
-			throw new IllegalArgumentException("org id is required");
-		}
-
+	public List<EmployeeWithOrgNameResponse> getEmployeesByOrg(String orgId) {
 		return employeeRepository.findByOrgId(orgId)
-			.stream().map(employeeMapper::toResponse).toList();
+			.stream().map(employeeMapper::toResponseWithOrgName).toList();
 	}
 
-	public List<EmployeeResponse> getEmployeesByOrgList(List<String> orgId) {
+	public List<EmployeeWithOrgNameResponse> getEmployeesByOrgList(List<String> orgId) {
 		if (orgId == null) {
 			throw new IllegalArgumentException("org id is required");
 		}
-		var result = employeeRepository.findByOrgIdIn(orgId)
-			.stream().map(employeeMapper::toResponse).toList();
 
-		if (result.isEmpty()) {
+		var employees = employeeRepository.findByOrgIdIn(orgId);
+
+		if (employees.isEmpty()) {
 			throw Problem.valueOf(Status.NOT_FOUND);
 		}
 
-		return result;
+		return employees.stream().map(employeeMapper::toResponseWithOrgName).toList();
 	}
 
-	public List<EmployeeResponse> getAllEmployees() {
-		return employeeRepository.findAll().stream().map(employeeMapper::toResponse).toList();
+	public List<EmployeeWithOrgNameResponse> getAllEmployees() {
+		return employeeRepository.findAll().stream().map(employeeMapper::toResponseWithOrgName).toList();
 	}
 
 }
