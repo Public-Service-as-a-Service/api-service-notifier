56535dab9e1bedbcf376767eea6d825431aee1e1
Simon-n-o <simon.n.olsson@gmail.com>
2026-02-09 11:16:02 +0100

    #31 Adds missing response

--- src/main/java/se/sundsvall/notifier/api/OrganizationResource.java
+++ src/main/java/se/sundsvall/notifier/api/OrganizationResource.java
@@ -41,7 +41,11 @@ public class OrganizationResource {
 		this.organizationService = organizationService;
 	}
 
-	@Operation(summary = "Get spicific organization")
+	@Operation(summary = "Get spicific organization", responses =
+		@ApiResponse(
+				responseCode = "404",
+				description = "Not Found",
+				content = @Content(schema = @Schema(implementation = Problem.class))))
 	@GetMapping("/{orgId}")
 	public ResponseEntity<OrganizationResponse> getSpecificOrganization(@PathVariable String orgId) {
 		return ResponseEntity.ok(organizationService.getSpecificOrg(orgId));
