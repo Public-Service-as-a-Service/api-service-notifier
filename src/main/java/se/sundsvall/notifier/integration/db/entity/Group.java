83f4c2f0c063ad9ea2fc15d5b6e08139eb5d1114
Joakim Ring <joakim.ring@icloud.com>
2026-02-10 14:37:47 +0100

    #39 Removed Member/memberId, updated entities, updated flywayscript
    # Conflicts:
    #	src/main/java/se/sundsvall/notifier/integration/db/entity/Message.java
    #	src/main/resources/db/migration/V1_0__initial.sql

--- src/main/java/se/sundsvall/notifier/integration/db/entity/Group.java
+++ src/main/java/se/sundsvall/notifier/integration/db/entity/Group.java
@@ -1,6 +1,5 @@
 package se.sundsvall.notifier.integration.db.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,8 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -17,16 +16,20 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder(setterPrefix = "with")
 @Entity
 @Table(name = "user_group")
-public class UserGroup {
+public class Group {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,12 +42,16 @@ public class UserGroup {
 	@Column(name = "description", columnDefinition = "TEXT")
 	private String description;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "creator_employee_id", nullable = false)
-	private Employee creator;
+	@Column(name = "creator_id", nullable = false, updatable = false, length = 255)
+	private String creatorId;
 
-	@OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
-	private Set<Member> members = new HashSet<>();
+	@Builder.Default
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+		name = "employee_user_group",
+		joinColumns = @JoinColumn(name = "group_id"),
+		inverseJoinColumns = @JoinColumn(name = "employee_id"))
+	private Set<Employee> employees = new HashSet<>();
 
 	@Column(name = "created_at", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
@@ -67,70 +74,13 @@ public class UserGroup {
 		updatedAt = LocalDateTime.now();
 	}
 
-	public static UserGroup create() {
-		return new UserGroup();
-	}
-
-	public void addEmployee(Employee employee) {
-		if (employee == null)
-			return;
-
-		boolean alreadyMember = members.stream()
-			.anyMatch(m -> m.getEmployee() != null && m.getEmployee().equals(employee));
-
-		if (alreadyMember)
-			return;
-
-		Member member = Member.create()
-			.withEmployee(employee)
-			.withGroup(this);
-
-		members.add(member);
-
-		employee.addMembership(member);
-	}
-
-	public boolean removeEmployee(Employee employee) {
-		if (employee == null)
-			return false;
-
-		for (var it = members.iterator(); it.hasNext();) {
-			Member m = it.next();
-			if (employee.equals(m.getEmployee())) {
-				it.remove();
-				employee.getMemberships().remove(m);
-
-				m.setGroup(null);
-				m.setEmployee(null);
-
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public UserGroup withName(String name) {
-		this.name = name;
-		return this;
-	}
-
-	public UserGroup withDescription(String description) {
-		this.description = description;
-		return this;
-	}
-
-	public UserGroup withCreator(Employee creator) {
-		this.creator = creator;
-		return this;
-	}
-
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
 			return true;
-		if (!(o instanceof UserGroup userGroup))
+		if (!(o instanceof Group group))
 			return false;
-		return id != null && Objects.equals(id, userGroup.id);
+		return id != null && Objects.equals(id, group.id);
 	}
 
 	@Override
@@ -140,11 +90,11 @@ public class UserGroup {
 
 	@Override
 	public String toString() {
-		return "UserGroup{" +
-			"id=" + id +
-			", name='" + name + '\'' +
+		return "Group{" +
+			"name='" + name + '\'' +
+			", id=" + id +
 			", description='" + description + '\'' +
-			", creator=" + creator +
+			", creatorId='" + creatorId + '\'' +
 			", createdAt=" + createdAt +
 			", updatedAt=" + updatedAt +
 			'}';
